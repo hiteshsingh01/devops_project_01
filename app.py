e85f4b0daedb2f1b3304aa40be6c5f026a46ca09
hiteshsingh01 <hiteshsingh2k4@gmail.com>
2025-07-06 23:37:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/info")
+def lwinfo():
+	return "This Is Hitesh Singh"
+
+@app.route("/phone")
+def lwphone():
+	return "9999999999"
+
+app.run(host="0.0.0.0")
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import lwphone
+
+def test_lwphone():
+	assert lwphone() == "9999999999"
